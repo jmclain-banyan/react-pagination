2c6d027c1197f6ae4642cb5fbd7994866bbea093
joshmclain45 <83558840+joshmclain45@users.noreply.github.com>
2021-06-14 10:12:38 -0700

    implemented pagination logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import data from "./data.json";
+import { Data } from "./users";
 import User from "./components/User";
+import { paginate } from "./components/Pagination/paginate";
+import Pagination from "./components/Pagination";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 8;
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  const getPageData = () => {
+    const paginationData = paginate(Data, currentPage, pageSize);
+    return { totalCount: Data.length, data: paginationData };
+  };
+
+  const { totalCount, data } = getPageData();
+
   return (
     <div className="App">
       <h1>ALL USERS</h1>
-      {data.users.map((user, index) => (
+      {data.map((user, index) => (
         <User key={index} name={user.first_name} email={user.email} />
       ))}
+      <Pagination
+        itemsCount={totalCount}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
