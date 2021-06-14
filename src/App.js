da280bf18698e80d0a0c9b8d19697e54c9d14c70
joshmclain45 <83558840+joshmclain45@users.noreply.github.com>
2021-06-14 09:54:12 -0700

    added pagination component and paginate function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import data from "./data.json";
+import User from "./components/User";
 
 function App() {
   return (
     <div className="App">
-     
+      <h1>ALL USERS</h1>
+      {data.users.map((user, index) => (
+        <User key={index} name={user.first_name} email={user.email} />
+      ))}
     </div>
   );
 }
